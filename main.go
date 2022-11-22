d50cde52de756db76305d33b6758122f60a501b3
furkansade <20080069@stu.omu.edu.tr>
2022-11-22 10:40:34 +0300

    create-handlefunc

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"html/template"
+	"net/http"
+)
+
 func main() {
 
+	http.HandleFunc("/", Home)
+	http.ListenAndServe(":9090", nil)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	view, _ := template.ParseFiles("index.html")
+	view.Execute(w, nil)
 }
